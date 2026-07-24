7d56a8c75b446050bb06648714aa97abd6aad8ce
X_huihui <2519350300@qq.com>
2026-07-24 14:22:12 +0800

    init

---
+++ src/main/java/top/mpt/huihui/pvprank/PVPRank.java
@@ -0,0 +1,17 @@
+package top.mpt.huihui.pvprank;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PVPRank extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
